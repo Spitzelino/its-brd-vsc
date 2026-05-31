b45c18ec9f3d836ea42c2435a8fdd0238195e8d8
Spitzelino <jan.klindt@icloud.com>
2026-05-31 16:39:01 +0200

    Aufgabe 5 überarbeitet und Aufgabe 6 fertig

--- Programs/PrimzahlSiebASM/Src/main.s
+++ Programs/PrimzahlSiebASM/Src/main.s
@@ -6,81 +6,180 @@
 ;   Xuan Hoang Duy Trinh Matrikel-Nr. 2881544
 ;   Jan Klindtworth      Matrikel-Nr. 2884053
 ;************************************************
-;________________________________________________
-;		       	Assambler Code
-;________________________________________________
-    AREA MyData, DATA, align = 2
-Basis       EQU 0x3e8
-Grenze      FILL Basis + 1
 
+
+;------------------------------------------------
+;		       	Rigister Zuweise
+;------------------------------------------------
+
+
+                AREA MyData, DATA, align = 2
+Grenze       EQU    0x3E8
+Basis        FILL   Grenze + 1
+Prim         FILL   0x7D0
 
     AREA |.text|, CODE, READONLY, ALIGN = 3
+        		EXPORT main
+        		EXTERN initITSboard
+
+main            PROC
+
+
+;------------------------------------------------
+;		       	Assambler Code
+;------------------------------------------------
+
     
-    EXTERN initITSboard
-    EXPORT main
-main PROC
-    ldr R0, =Grenze 
-    mov R1, #1000      
-; Initialisierung: p[0] = 0, p[1] = 0, p[2] = 1
-    mov R2, #0
-    strb R2, [R0, #0]       ; p[0] = 0
-    strb R2, [R0, #1]       ; p[1] = 0
-for1                        ; für i=2 bis n=1000 alles als Primzahl betrachtet wird
-    mov R4, #1
-    strb R4, [R0, R2]       ; p[2] = 1 aber der inhalt von R2 = 0
-    mov R2, #2              ; R2 = i = 2 also i = 2 Schleife startet bei 2
-until1
-    cmp R2, R1              ; vergleich von i zu n
-    bhi for2                ; wenn i > 1000 dann ist Schleife fertig
-    mov R4, #1
-    strb R4, [R0, R2]       ; p[i] = 1 also könnte eine Primzahl sein
-    add R2, R2, #1          ; i++
-    b for1                  ; geh wieder zurück zu for1
-for2
-    mov R2, #2              ;  R2 = i = 2 (äußere Schleife neustarten)
-unitl2
-    cmp R2, R1              ; schauen ob i<=1000 ist
-    bhi endfor1             ; wenn i>1000 ist schleife fertig
-do2
-    mul R7, R2, R2          ; R7 = i*i (abbruchbedingung der innerren Schleife)
-    cmp R7, R1              ; ist i*i <= 1000?
-    bhi step1               ; wenn i*i > 1000 dann nicht mehr 7 (Sieben hahahhahaha) ->(alle Vielfachen wurden schon gefunden)
-if1
-    ldrb R4, [R0, R2]       ; R4 = p[i] -> Byte an Adresse R0 + i lesen
-    cmp R4, #1              ; ist p[i] == 1 wahr?
-    bne step1               ; Wenn p[i] == 0 dann ist i keine Primzahl
-for3
-    mul R3, R2, R2          ; R3 = j = i*i (Start der inneren Schleife)
-    cmp R3, R1              ; ist j <= 1000?
-    bhi step1               ; wenn j > 1000 dann zurück auf die äußere Schleife
-do3
-    mov R6, #0              ; R6 = 0 (R6 ist keine Primzahl)
-    strb R6, [R0, R3]       ; p[j] = 0 Vielfaches von i wird markiert (gestrichen).
-    add R3, R3, R2          ; j += i nächste Vielfaches von i
-    b for3                  ; zurück zum Kopf/Anfang/Start der inneren Schleife
-step1
-    add R2, R2, #1          ; i++ -> nächste Zahl Prüfen
-    b until1                ; zurück zum Kopf der äußeren Schleife
-endfor1                    ; Ende ( das 7 ist fertig)
+            ldr     R0,=Basis
+            ldr     R1,=Grenze
+for_init
 
-for_zaehler
-    mov R2, #2              ; R2 = i = 2 (von vorne durchlaufen)
-    mov R5, #0              ; R5 = 0 (Zähler beginnt bei null obvie)
-until_zaehler
-    cmp R2, R1              ; i <= 1000 ?
-    bhi zaehler_fertig      ; wenn i > 1000 dann ist Zähler fertig
+            mov     R2, #2
+            mov     R4, #1
 
-    ldrb R4, [R0, R2]       ; R4 = p[i] aktuellen Eintag lesen
-    cmp R4, #1              ; p[i] == 1 ? (ist das ne Primzahl?)
-    bne zaehler_sprung      ; wenn p[i] == 0 -> dann ist keine Primzahl
-    add R5, R5, #1          ; R5++ (R5 + 1) -> Primzahl gefunden yay also counter erhöhen
-zaehler_sprung
-    add R2, R2, #1          ; i++ (nächsten eintrag Prüfen)
-    b until_zaehler         ; zurück zum SchleifenKopf/Anfang
-zaehler_fertig
-    str R5, [R0,#1004]      ; Speicheret die end Anzahl der Primzahlen im Speicher an der Stelle R5
+until_init
+
+            cmp     R2, R1
+            bhi     end_init
+
+do_init
+
+            strb    R4, [R0, R2]
+
+step_init 
+
+            add     R2, #1
+            b       until_init
+
+end_init
+
+;------------------------------------------------
+;               äußere Schleife
+;------------------------------------------------
+
+for_7
+
+            mov     R2, #1
+            mov     R6, #0
+
+untilfor_7
+
+            mul     R7, R2, R2
+            mov     R3, R7
+            cmp     R3, R1
+            bhi     endfor_7
+
+dofor_7
+
+            ldrb    R4, [R0,R2]
+            b       if_7
+
+stepfor_7
+
+            b       naechster
+
+endfor_7
+
+            b       abspeichern
 
 
+if_7
+
+            cmp     R4, #1
+            beq     thenif_7
+            b       endif_7
+
+thenif_7
+
+            b       for_streichen
+
+endif_7
+
+            b       naechster
+
+
+;------------------------------------------------
+;               innere Schleife
+;------------------------------------------------
+
+
+for_streichen
+
+until_streichen
+
+            cmp     R3, R1
+            bhi     endfor_streichen
+
+dofor_streichen
+
+            strb    R6, [R0, R3]
+
+stepfor_streichen
+
+            add     R3, R2
+            b       until_streichen
+
+endfor_streichen   
+
+            b       naechster
+
+
+;------------------------------------------------
+;                   quasi ++i
+;------------------------------------------------
+
+
+naechster
+
+            add     R2, #1
+            b       untilfor_7
+
+
+;------------------------------------------------
+;                  Abspeichern
+;------------------------------------------------
+
+
+abspeichern
+
+            ldr     R5, =Prim
+
+for_abspeichern
+
+            mov     R2, #2
+
+untilfor_abspeichern
+
+            cmp     R2, R1
+            bhi     endfor_abspeichern
+        
+dofor_abspeichern
+
+            ldrb    R4, [R0, R2]
+            b       if_abspeichern
+    
+stepfor_abspeichern
+
+endfor_abspeichern
+
+            b       forever
+
+if_abspeichern
+
+            cmp     R4, #1
+            beq     thenif_abspeichern
+            b       endif_abspeichern
+
+thenif_abspeichern
+
+            str     R2, [R5]
+            add     R5, #4
+            b       endif_abspeichern
+
+endif_abspeichern
+
+            add     R2, #1
+            b       untilfor_abspeichern
 
 
 
